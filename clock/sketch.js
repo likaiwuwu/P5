b0fe0c6f6a31b72bd90e99f18866eefc76b06bd1
lw829 <lw829@uw.edu>
2019-05-27 22:23:09 -0700

    implemented normal clock with bars

--- clock/sketch.js
+++ clock/sketch.js
@@ -12,22 +12,41 @@ function draw() {
 	let mn = minute();
 	let sc = second();
 
-	//fill(255);
-	//noStroke();
-	//text(hr + ":" + mn + ":" + sc, 10, 200);
-
 	strokeWeight(8);
 	stroke(255, 100, 150);
 	noFill();
-	let end1 = map(sc, 0, 60, 0, 360);
-	arc(0, 0, 300, 300, 0, end1);
+	let secondAngle = map(sc, 0, 60, 0, 360);
+	arc(0, 0, 300, 300, 0, secondAngle);
 
+	
 	stroke(150, 100, 255);
-	let end2 = map(mn, 0, 60, 0, 360);
-	arc(0, 0, 280, 280, 0, end2);
+	let minuteAngle = map(mn, 0, 60, 0, 360);
+	arc(0, 0, 280, 280, 0, minuteAngle);
 
 	stroke(150, 255, 100);
-	let end3 = map(hr % 12, 0, 12, 0, 360);
-	arc(0, 0, 260, 260, 0, end3);
+	let hourAngle = map(hr % 12, 0, 12, 0, 360);
+	arc(0, 0, 260, 260, 0, hourAngle);
+	
+	push();
+	rotate(secondAngle);
+	stroke(255, 100, 150);
+	line(0, 0, 100, 0);
+	pop();
+
+	push();
+	rotate(minuteAngle);
+	stroke(150, 100, 255);
+	line(0, 0, 75, 0);
+	pop();
+
+	push();
+	rotate(hourAngle);
+	stroke(150, 255, 100);
+	line(0, 0, 50, 0);
+	pop();
+
+	stroke(255);
+	point(0, 0);
+
 }
 
